40ad122fb4dba8ea1862acf096797e2f3a57fb0f
Adrien Thebo <athebo@polysync.io>
2018-03-21 11:18:53 -0700

    Implement capability query

--- src/parser.rs
+++ src/parser.rs
@@ -2,7 +2,11 @@
 
 pub const START_SYSEX: u8 = 0xF0;
 pub const END_SYSEX: u8 = 0xF7;
+
 pub const QUERY_FIRMWARE: u8 = 0x79;
+
+pub const CAPABILITY_QUERY: u8 = 0x6B;
+pub const CAPABILITY_RESPONSE: u8 = 0x6c;
 
 use nom::IResult;
 
@@ -15,8 +19,14 @@ pub enum SysexMsg<'a> {
         major: Option<&'a u8>,
         minor: Option<&'a u8>,
         firmware_name: Option<&'a [u8]>
-    }
+    },
+    CapabilityQuery,
 }
+
+
+named!(capability_query<&[u8], SysexMsg>,
+       map!(tag!(&[CAPABILITY_QUERY]), |_| SysexMsg::CapabilityQuery));
+
 
 
 named!(query_firmware<&[u8], SysexMsg>,
@@ -46,7 +56,10 @@ named!(query_firmware<&[u8], SysexMsg>,
 named!(sysex<&[u8], SysexMsg>,
        delimited!(
            tag!(&[START_SYSEX]),
-           query_firmware,
+           alt!(
+               query_firmware |
+               capability_query
+           ),
            tag!(&[END_SYSEX])
        )
 );
@@ -89,6 +102,16 @@ mod tests {
                     firmware_name: Some(&b"StandardFirmata.ino"[..]),
                 }
             )
+        );
+    }
+
+    #[test]
+    fn parses_sysex_capability_query() {
+        let msg = b"\xF0\x6B\xF7";
+
+        assert_eq!(
+            sysex(&msg[..]),
+            IResult::Done(EMPTY, SysexMsg::CapabilityQuery)
         );
     }
 }
