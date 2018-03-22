fd4a513f33a8a11c126ac6decee55bb2ee4dcb06
Adrien Thebo <athebo@polysync.io>
2018-03-22 10:07:44 -0700

    Don't use glob imports in example code

--- examples/report_version.rs
+++ examples/report_version.rs
@@ -1,18 +1,18 @@
 extern crate firmata;
 extern crate serial;
 
-use serial::*;
+use serial::SerialPort;
 
 fn main() {
     let device = "/dev/ttyACM0";
     let mut sp = serial::open(device).expect(&format!("Unable to open serial device"));
 
     sp.reconfigure(&|settings| {
-        settings.set_baud_rate(Baud57600).unwrap();
-        settings.set_char_size(Bits8);
-        settings.set_parity(ParityNone);
-        settings.set_stop_bits(Stop1);
-        settings.set_flow_control(FlowNone);
+        settings.set_baud_rate(serial::Baud57600).unwrap();
+        settings.set_char_size(serial::Bits8);
+        settings.set_parity(serial::ParityNone);
+        settings.set_stop_bits(serial::Stop1);
+        settings.set_flow_control(serial::FlowNone);
         Ok(())
     }).expect("Unable to reconfigure serial device");
 }
